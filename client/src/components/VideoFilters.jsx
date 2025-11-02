a1f064c8c6f5414038464d6a7f83f70b43068484
Sean Jefferies <seanjefferies@Seans-MacBook-Pro.local>
2025-11-02 00:44:02 -0600

    saving work so far

--- client/src/components/VideoFilters.jsx
+++ client/src/components/VideoFilters.jsx
@@ -1,9 +1,25 @@
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel'
 import './VideoFilters.css';
 
 const VideoFilters = () => {
     return (
         <div id="video-filters">
+            
+            <h3>Filters</h3>
 
+            <FormControl component="fieldset" variant="standard">
+                <FormLabel component="legend">Categories</FormLabel>
+                <FormGroup>
+                    <FormControlLabel control={<Checkbox />} label="Cooking" />
+                    <FormControlLabel control={<Checkbox />} label="DIY" />
+                    <FormControlLabel control={<Checkbox />} label="Fitness" />
+                    <FormControlLabel control={<Checkbox />} label="Travel" />
+                </FormGroup>
+            </FormControl>
         </div>
     )
 }
